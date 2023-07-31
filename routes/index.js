f8670ee739c577972dbcc1fe2d96652e7310dc8b
estydaven <esetnod1111@gmail.com>
2023-07-31 13:49:15 +0300

    delete unused route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,5 @@ const router = require('express').Router();
 router.use('/users', require('./users'));
 router.use('/tasks', require('./tasks'));
 router.use('/tests', require('./tests'));
-router.use('/screens', require('./screens'));
 
 module.exports = router;
